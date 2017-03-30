aab9a95e717fd3807aae3679da789b127921f954
Nina Kin <nina@ninakin.com>
2017-03-30 01:53:16 +0000

    add markers for top ten art

--- front/script.js
+++ front/script.js
@@ -1,11 +1,25 @@
 /*global $*/
 /*global L*/
-/*global firebase*/
 
 /*
  * Initialize Map
  */
 L.mapbox.accessToken = 'pk.eyJ1IjoibWF0aWtpbjkiLCJhIjoiYjMyMjBjZTE4NWUxMDkxOWZjZjFjZWEzZTcxNDUxOTkifQ._ldFl3e17jCs7aWm6zMZ3Q';
-var mymap = L.map('map-display').setView([34.0522, -118.2437], 13);
+var mymap = L.map('map-display').setView([34.15, -118.24], 9);
 L.mapbox.styleLayer('mapbox://styles/matikin9/cim5bt1q100iy9jkpl7ff9d1h').addTo(mymap);
 
+$.getJSON('../front/toptenv1.json', function(data) {
+    $.each( data, function( key, val ) {
+        var $popup = $("<div>", { 
+        	"id": key
+        });
+        
+        // Create element with restaurant info.
+        $popup.append("<h1>" + val.title + "</h1>");
+        $popup.append("<p>" + val.desc.substring(0, 100) + "..." + "</p>");
+        
+        var m = L.marker([val.lat, val.lng])
+            	 .bindPopup($popup[0])
+            	 .addTo(mymap);
+    });
+});
