0d7854d92ba7304e7fe6a51163c26de1155106a5
xtine <christine@fieryorange.com>
2017-06-16 16:57:19 -0600

    set up basic templates and css

--- civicart/settings.py
+++ civicart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$s_4-(v=9td&!t9m4zt*25@#%^yj79xvr$j6ibq_@+b@gef(ql'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
