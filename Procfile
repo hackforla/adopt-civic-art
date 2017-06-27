96c26b557bea1778043b432f4e0b3eb7a9260a90
xtine <christine@fieryorange.com>
2017-06-27 00:26:35 -0400

    deployment files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn adopt-civic-art.wsgi
--- civicart/settings.py
+++ civicart/settings.py
@@ -134,3 +134,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
