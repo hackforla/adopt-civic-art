5cada6fd4d5860f633eeaace3f63ae0cd3e401b6
xtine <christine@fieryorange.com>
2017-07-13 21:48:21 -0700

    update aws bucket url

--- civicart/settings.py
+++ civicart/settings.py
@@ -127,6 +127,6 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
-    MEDIA_URL = 'http://%s.s3.amazonaws.com/your-folder/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
