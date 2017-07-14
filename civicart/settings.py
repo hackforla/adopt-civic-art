9098adede0330c94e504878bfe2b7ebd6e8709f9
xtine <christine@fieryorange.com>
2017-07-13 21:39:31 -0700

    set logging

--- civicart/settings.py
+++ civicart/settings.py
@@ -75,6 +75,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
 
 STATIC_URL = '/static/'
 
