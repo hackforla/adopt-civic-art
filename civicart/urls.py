f38ffeccf29281a9b7be0b5425e25366d7270b27
xtine <christine@fieryorange.com>
2017-10-24 19:59:20 -0700

    about page

--- civicart/urls.py
+++ civicart/urls.py
@@ -1,13 +1,14 @@
-
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from artworks import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^accounts/profile', views.profile),
     url(r'^artwork/(?P<id>[\w-]+)$', views.artwork, name='artwork'),
